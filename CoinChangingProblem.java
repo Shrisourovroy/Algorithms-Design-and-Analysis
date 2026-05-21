7fb9fbfede783c5a4875d1644fe2a77d7ec3bf54
Sourov Roy <sspproy222@gmail.com>
2026-05-22 04:33:14 +0600

    Add files via upload

---
+++ CoinChangingProblem.java
@@ -0,0 +1,36 @@
+
+package com.mycompany.coinchangingproblem;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class CoinChangingProblem {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count=0;
+        System.out.println("Enter available coin number: ");
+        int n = sc.nextInt();
+        System.out.println("Enter available coin: ");
+        int [] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println("Enter Total amount: ");
+        int m = sc.nextInt();
+        for (int i = n-1; i >=0; i--) {
+            if(m%arr[i]==0){
+                count = count + m/arr[i];
+            }
+            else
+            {
+                count = count +m/arr[i];
+                m = m%arr[i];
+            }
+        }
+        System.out.println("<Minimum coin need: "+count);
+        
+    }
+}
